76e93160b6f2c991b7caec910feecd25e34da66b
Sanil-Surve <sanilsurve42@gmail.com>
2023-05-23 13:20:19 +0530

    file changed

--- p6.cpp
+++ p6.cpp
@@ -1,7 +1,30 @@
 #include<iostream>
 using namespace std;
 
+int sum(int a, int b); //function declaration
+
+
+
 int main() 
 {
-    
+    int num1, num2, total = 0;
+    cout << "Enter the first Number: " << endl;
+    cin >> num1;
+
+    cout << "Enter the second the number: " << endl;
+    cin >> num2;
+
+    total = sum(num1, num2); // function call
+
+    cout << "Total of " << num1 << " and " << num2 << " is " << total << endl;
+
+    return 0;
+
+}
+
+int sum(int a, int b) 
+{
+    int result;
+    result = a + b;
+    return result;
 }
